9b107eefae320834c8d810f599b0bf4197fd83f9
DeboraCristina <deboracristinaproficional1@gmail.com>
2022-05-04 13:02:37 +0200

    fix - variable error

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: desilva <deboracristinaproficional1@gma    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 19:33:00 by desilva           #+#    #+#             */
-/*   Updated: 2022/04/25 15:44:19 by desilva          ###   ########.fr       */
+/*   Updated: 2022/04/30 03:45:33 by desilva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(char *str)
+size_t	ft_strlen(const char *str)
 {
 	size_t	c;
 
